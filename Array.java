fdc1e1aaee8a7b405cf3b7cfdc6914a72fefd97f
ISHANT AUNCHALIYA <61375497+IshnatJain@users.noreply.github.com>
2022-10-01 23:58:47 +0530

    Basic_Array

---
+++ Array.java
@@ -0,0 +1,104 @@
+
+package javaClass;
+// 1 D ARRAY
+// public class Array {
+//    public static void main(String[] args) {
+//
+//        int [] A1={10,20,30};
+//        int [] A2={10,20,30};
+//        int sum1=0;
+//        int sum2=0;
+//        int total=0;
+//
+//
+//        System.out.println("1st Array is");
+//        for (int num1:A1) {
+//            sum1+=num1;
+//            System.out.println(num1);
+//
+//
+//        }
+//        System.out.println("2nd Array is");
+//        for (int num2:A2) {
+//            sum2+=num2;
+//            System.out.println(num2);
+//
+//
+//        }
+//        total=sum1+sum2;
+//
+//        System.out.println("SUM : " + total);
+//
+//    }
+//}
+
+// 2 D ARRAY
+// 2D array is collection of 1D Array
+
+//        int[][] myFirst2DArray = {
+//                { 3, 5, 1, 9 },
+//                { 10, 15, 3, 0  },
+//                { 1, 1, 1, 1 },
+//                { 1, 1, 1, 1 }
+//        };
+//        System.out.println(myFirst2DArray[1][2]);
+//        int total=0;
+//        int sum1=0;
+//        int column=0;
+//        for (int i=0;i<myFirst2DArray.length;i++) {
+//            for (int j = 0; j < myFirst2DArray[i].length; j++) {
+////                System.out.println(myFirst2DArray[i][j]);
+//                total = total + myFirst2DArray[i][j];
+//            }
+//
+//            System.out.println(total);
+//        }
+//
+//        for (int r = 0; r < myFirst2DArray.length; r++) {
+//
+//            for (int c=0;r<myFirst2DArray[r].length;c++){
+//                column= column + myFirst2DArray[c][r];
+//
+//
+//            }
+//            column=0;
+//            System.out.println(column);
+//        }
+
+//        for (int row = 0; row < myFirst2DArray.length; row++) {
+//
+//            for (int column = 0; column < myFirst2DArray[row].length; column++) {
+//                System.out.print(myFirst2DArray[row][column] + " ");
+//            }
+//
+//            System.out.println();
+//        }
+
+//        String s1="i am jolly";
+//        System.out.println(s1.contains("jo"));
+//        System.out.println(s1.charAt(3));
+//        int x=5;
+//        if (x != 0){
+//            int y = x+1;
+//        }
+//        System.out.println(y);
+
+
+//public class Array {
+//
+//    public static void main(String[] args) {
+//
+//               check();
+//        System.out.println(check());
+//    }
+//
+//    public static int check() {
+//        int a, b,sum;
+//        a=10;
+//        b=10;
+//
+//        return a+b;
+//    }
+//}
+
+
